93b0b4c0df1db680850a315f4830bdceca6bd59c
Pablo Josue Noack Cajbon <noa17596@uvg.edu.gt>
2021-03-15 14:44:08 -0600

    Puse variables cuantitativas y empiezo con otras cosas

--- Arbolites de desición.R
+++ Arbolites de desición.R
@@ -1,8 +1,15 @@
 setwd("C:/Users/LENOVO/Desktop/Clases/Minería de datos/Github/HT3.Arboles_de_decision")
 library(corrplot)
 library(nortest)
+library(cluster) 
+library(e1071)
+library(mclust) 
+library(fpc) 
+library(NbClust) 
+library(factoextra)
+library(hablar)
 data <- read.csv("house-prices-advanced-regression-techniques/test.csv")
-
+cuantitativas <- data.frame(data$Id)
 
 #data <-na.omit(data) #data[TRUE !=is.na(data),]
 
@@ -47,6 +54,7 @@ for (val in nombres  ){
     qqnorm(data[,val], pch = 1, frame = FALSE,main = val)
     qqline(data[,val], col = "steelblue", lwd = 2)
     print(lillie.test(data[,val]))
+    
     }
 }
 
@@ -55,9 +63,24 @@ for (val in nombres  ){
 
 bins=floor(sqrt(length(na.omit(data$X1stFlrSF))))
 h1<-hist(data$X1stFlrSF,  breaks = bins, plot = FALSE)
-bins=floor(sqrt(length(na.omit(data$LotArea))))
-h2<-hist(data$LotArea, breaks = bins,plot = FALSE)
 
-plot(h2)
-plot(h1, add = TRUE)
+bins=floor(sqrt(length(na.omit(data$X2ndFlrSF))))
+h2<-hist(data$X2ndFlrSF, breaks = bins,plot = FALSE)
+
+plot(h2,col  = rgb(0.1,0.7,0.2,0.3), xlab = "m^2", main = " Comparación de  áreas entre el primer y segundo nivel")
+plot(h1, add = TRUE, col=rgb(0.7, 0.2, 0.1, 0.3))
+legend("topright", 
+       c("2nd floor", "1st floor"), 
+       lty=c(1, 2), 
+       col=c(rgb(0.1,0.7,0.2,0.3),rgb(0.7, 0.2, 0.1, 0.3)), 
+       bty = "n")
+
+nums <- sapply(data, is.numeric)
+cuantitativas <- data[ , nums]
+cuantitativas<-na.omit(cuantitativas)
+###### Corrplot 
+cuantitativas$data.Id<- NULL
+cuantitativas$Id <- NULL
+cuantitativas<-na.omit(cuantitativas)
+corrplot(cuantitativas,use="complete.obs")
 
